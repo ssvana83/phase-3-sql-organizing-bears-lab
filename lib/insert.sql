d434810c6007f023be3c30ea601a43bec8167b1a
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-10 10:04:08 -0500

    commit tree 2f219a2cf1821aae2c02d5f8275af3ca703452c7

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex) VALUES ('Mr. Chocolate', 'male');
+INSERT INTO bears (name, sex) VALUES ('Rowdy', 'male');
+INSERT INTO bears (name, sex) VALUES ('Tabitha', 'female');
+INSERT INTO bears (name, sex) VALUES ('Sergeant Brown', 'male');
+INSERT INTO bears (name, sex) VALUES ('Melissa', 'female');
+INSERT INTO bears (name, sex) VALUES ('Grinch', 'male');
+INSERT INTO bears (name, sex) VALUES ('Wendy', 'female');
+INSERT INTO bears (name, sex) VALUES (NULL, 'female');
+
